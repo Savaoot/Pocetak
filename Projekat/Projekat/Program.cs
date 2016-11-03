116d98a4cbd62cdc5a6c5d1aa117998cc8e59edd
Savaoot <mirol@DESKTOP-3CDMEB4>
2016-11-03 01:31:25 +0100

    Vjezbao sam

--- Projekat/Projekat/Program.cs
+++ Projekat/Projekat/Program.cs
@@ -10,7 +10,26 @@ namespace Projekat
     {
         static void Main(string[] args)
         {
-            
+            string unos;
+            Osoba o = new Osoba("nikola", "Nikolic", "1535456");
+            Osoba os = new Osoba();
+
+            Console.WriteLine("Unesite ime");
+            unos = Console.ReadLine();
+            os.Ime = unos;
+
+            Console.WriteLine("Unesite prezime");
+            unos = Console.ReadLine();
+            os.Prezime = unos;
+
+            Console.WriteLine("Unesite jebi me");
+            unos = Console.ReadLine();
+            os.JMBG = unos;
+
+            Console.WriteLine("Podaci o osobi su: ");
+            Console.WriteLine(os.Ime + " " + os.Prezime + " " + os.JMBG);
+            Console.WriteLine(os);
+
         }
     }
 }

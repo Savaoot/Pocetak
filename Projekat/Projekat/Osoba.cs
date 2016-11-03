958a526f845bd2a4c8999eb1d0349bcea6295565
Savaoot <mirol@DESKTOP-3CDMEB4>
2016-11-03 01:30:11 +0100

    Kompletirao klasu Osoba

--- Projekat/Projekat/Osoba.cs
+++ Projekat/Projekat/Osoba.cs
@@ -8,6 +8,28 @@ namespace Projekat
 {
    public class Osoba
     {
+        // Konstruktori
+        public Osoba()
+        {
+        }
+
+        public Osoba(string ime, string prezime, string jmbg)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            JMBG = jmbg;
+        }
+
+        public string Ime { get; set; }
+
+        public string Prezime { get; set; }
+
+        public string JMBG { get; set; }
+
+        public override string  ToString()
+        {
+            return Ime + " " + Prezime + " " + JMBG;
+        }
 
     }
 }
